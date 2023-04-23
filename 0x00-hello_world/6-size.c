0a84980d8aedbd292fd33a5c7d2cb53ac178b2b4
yasmeen53 <yasmeen53@github.com>
2023-04-22 21:30:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char: %d byte(s)" Sizeof(char));
+printf("Size of an int: %d byte(s)" Sizeof(int));
+printf("Size of a long int: %d byte(s)" Sizeof(long int));
+printf("Size of a long long int: %d byte(s)" Sizeof(long long int));
+printf("Size of a float: %d byte(s)" Sizeof(float));
+return (0);
+}
