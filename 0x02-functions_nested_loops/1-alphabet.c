d4539b8409f8d3ceef20e8219a9e610ba4ab6745
yasmeen53 <128871760+yasmeen53@users.noreply.github.com>
2023-07-19 03:21:37 +0300

    Add files via upload
    Signed-off-by: yasmeen53 <128871760+yasmeen53@users.noreply.github.com>

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print english alphabet from a to z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
